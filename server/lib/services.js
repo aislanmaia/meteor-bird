34b6b031622e4c2faf2ef8d9189590b1e5f9a7d4
Aislan Maia <aislan.sousamaia@gmail.com>
2014-05-23 01:19:18 -0300

    autenticacao implementada, faltando appId e secret serem adicionados.

---
+++ server/lib/services.js
@@ -0,0 +1,6 @@
+ServiceConfiguration.configurations.remove({});
+ServiceConfiguration.configurations.insert({
+  service: 'facebook',
+  appId: 'inserir app gerado pelo Facebook',
+  secret: 'inserir secret gerado pelo Facebook'
+});
